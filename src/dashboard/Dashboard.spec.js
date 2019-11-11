dbe124550c95024f64be1af250cd7f55750ff1e1
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-11-11 15:41:10 -0700

    mvp done, 9 working tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from './Dashboard'
+
+test('Dashboard renders as expected', () => {
+    const dash = rtl.render(<Dashboard/>)
+    expect(dash.asFragment()).toMatchSnapshot()
+})
+
+test('Unlocked init', async () => {
+    const panel = rtl.render(<Dashboard/>)
+    await panel.findByText(/unlocked/i)
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import Display from './Display'
+
+test('Display renders as expected', () => {
+    const panel = rtl.render(<Display/>)
+    expect(panel.asFragment).toMatchSnapshot()
+})
+
+test('Displays closed if closed', () => {
+    const panel = rtl.render(<Display closed = {true}/>)
+    const closed = panel.getByText(/closed/i);
+    expect(closed).toHaveTextContent('Closed');
+})
+
+test('Init open', async () => {
+    const panel = rtl.render(<Display/>)
+    await panel.findByText(/open/i)
+})
+
+test('Init unlocked', async () => {
+    const panel = rtl.render(<Display/>)
+    await panel.findByText(/unlocked/i)
+})
